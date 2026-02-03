e0029cbd9dd86055b3398bf2cb50f164ac813a4f
Jay Suriya R <jaysuriya.rv@gmail.com>
2026-02-03 22:52:42 +0530

    Update resume_train.py

--- resume_train.py
+++ resume_train.py
@@ -5,12 +5,11 @@ def main():
     device = 0 if torch.cuda.is_available() else 'cpu'
     print(f"🚀 Using device: {'GPU' if device == 0 else 'CPU'}")
 
-    # Load your model
+
     model = YOLO('C:/Users/jaysu/OneDrive/Desktop/yol/runs/segment/train2/weights/last.pt')
 
-    # Resume training from the checkpoint
     model.train(
-        resume=True,  # ✅ this resumes training automatically
+        resume=True,
         device=device
     )
 
